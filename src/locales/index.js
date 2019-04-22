4899efd564b3813edacee16d116a0adda3743814
Daniel Oshiro <daniel.oshiro@webgoal.com.br>
2019-04-22 19:43:32 -0300

    Simple translation system

---
+++ src/locales/index.js
@@ -0,0 +1,6 @@
+import ptBRJSON from './pt-br.json'
+import enJSON from './en.json'
+
+export const ptBR = ptBRJSON
+
+export const en = enJSON
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
-
+import Vue from 'vue'
 import DefaultLayout from '~/layouts/Default.vue'
 import VueScrollTo from 'vue-scrollto'
 import Vuelidate from 'vuelidate'
@@ -8,6 +8,12 @@ import Vuelidate from 'vuelidate'
 import '@/assets/scss/main.scss'
 import '@/assets/css/page-transitions.css'
 import '@/assets/scss/bootstrap/bootstrap.scss'
+import { en, ptBR } from '@/locales'
+const locales = {
+  en,
+  ptBR
+}
+Vue.prototype.$locales = locales
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
