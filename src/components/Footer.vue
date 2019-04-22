66b9598f3e54b8fb8bb9df03407cc340aa02f122
Daniel Oshiro <daniel.oshiro@webgoal.com.br>
2019-04-22 13:29:27 -0300

    Address

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,8 @@
         <div class="col-md-3">
           <g-image src="/logo_atelie_footer.png" alt="Logo ateliê de software" />
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 vertical">
+          <address>Av. João Pinheiro, 341 - Sala 3 - Centro, Poços de Caldas - MG, 37701-880</address>
           <span tabindex="0">Todos os direitos reservados Ateliê de Software {{year}}©</span>
         </div>
       </div>
@@ -34,12 +35,25 @@ footer {
       }
     }
   }
-  span {
+  span,
+  address {
     color: #878f99;
     font-size: 15px;
     font-weight: 300;
     letter-spacing: 2.31px;
-    line-height: 13.07px;
+    line-height: 16px;
+  }
+  span {
+    font-size: 12px;
+    line-height: 12px;
+  }
+  .vertical {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    address {
+      margin-bottom: 10px
+    }
   }
 }
 </style>
