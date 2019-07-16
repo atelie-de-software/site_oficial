31d1216c89b4a4e27900533fbd84f66627a9f430
Daniel Oshiro <daniel.oshiro@webgoal.com.br>
2019-07-16 18:48:55 +0000

    Trocando endereço

--- locales/pt.js
+++ locales/pt.js
@@ -55,7 +55,7 @@ const pt = {
   },
   footer: {
     address:
-      'Av. João Pinheiro, 341 - Sala 3 - Centro, Poços de Caldas - MG, 37701-880 | (35) 3722-3776',
+      'R. Paraíba, 49 - Vila Nossa Sra. de Fatima, Poços de Caldas - MG, 37701-726 | (35) 3722-3776',
     copyright: 'Todos os direitos reservados Ateliê de Software 2019 ©'
   },
   heroCulture: {
