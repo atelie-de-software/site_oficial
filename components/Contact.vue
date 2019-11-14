f60774fc36360c43ca11384914a0a5deeee1d850
mateus andrade <mateus.souza@webgoal.com.br>
2019-11-14 14:36:29 -0300

    revert form de contato

--- components/Contact.vue
+++ components/Contact.vue
@@ -3,30 +3,7 @@
     <div class="container">
       <div class="row">
         <div class="col-md-6">
-          <div :class="socorro">
-            <script
-              src="https://s.pageclip.co/v1/pageclip.js"
-              charset="utf-8"
-            ></script>
-            <form
-              action="https://send.pageclip.co/5nqgwIYU1sEj0vS15XqCbt8FuFbN4EgZ/FormAtelie"
-              class="pageclip-form"
-              method="post"
-            >
-              <!-- Replace these inputs with your own. Make sure they have a "name" attribute! -->
-              <input type="text" name="name" value="Roscoe Jones" />
-              <input type="email" name="email" value="roscoe@example.com" />
-              <input type="text" name="phone" value="(35) 37832-4782" />
-
-              <!-- This button will have a loading spinner. Keep the inner span for best results. -->
-              <button type="submit" class="pageclip-form__submit">
-                <span>Enviar</span>
-              </button>
-            </form>
-          </div>
-        </div>
-        <div class="col-md-6">
-          R. Paraíba, 49 Vila Ricasavcuysavgcausygsyu, 37701-726 | (35)3722-3776
+          <Form />
         </div>
       </div>
     </div>
@@ -35,33 +12,16 @@
 
 <style lang="scss" scoped>
 .contact {
-  background-color: #ffffff;
-  color: rgb(0, 0, 0);
-  height: 80vh;
-}
-.block-scroller__Root-sc-23jz1-0 .tSSlS {
-  overflow-y: hidden !important;
-}
-
-.overflow-form {
-  overflow: hidden !important;
-}
-
-.typeform-widget {
-  height: auto;
-}
-.pageclip-form__success {
-  display: none;
+  background-image: url('../assets/images/map.png');
 }
 </style>
 
 <script>
+import Form from './Form.vue'
 export default {
   name: 'Contact',
-  data() {
-    return {
-      socorro: ''
-    }
+  components: {
+    Form
   }
 }
 </script>
