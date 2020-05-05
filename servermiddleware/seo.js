efb0ac05ec7e598b33c48f9160955b846db27771
Herbertt Bamonde <hpbamonde@gmail.com>
2020-05-05 15:12:10 -0300

    Digest adicionado na URL para evitar cache.

--- servermiddleware/seo.js
+++ servermiddleware/seo.js
@@ -3,8 +3,7 @@ const redirects = require('../301.json')
 module.exports = function (req, res, next) {
   const redirect = redirects.find(r => r.from === req.url)
   if (redirect) {
-    console.log(`redirect: ${redirect.from} => ${redirect.to}`)
-    res.writeHead(301, { Location: redirect.to })
+    res.writeHead(301, { Location: `${redirect.to}?timestamp=${(new Date()).getTime()}` })
     res.end()
   } else {
     next()
