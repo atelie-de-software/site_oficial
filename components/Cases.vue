55152dc3c6fe9bf196c02149ecf2e9f6ab7caca9
Jeff Albuquerque <jeff@MacBook-Pro-de-Jeff.local>
2020-06-04 15:49:27 -0300

    Comecando as alteracoes da section de cases.

--- components/Cases.vue
+++ components/Cases.vue
@@ -1,108 +1,37 @@
 <template>
   <div id="cases">
-    <div class="header-cases">
-      <h2 tabindex="0">
-        cases
-      </h2>
-    </div>
-
-    <Carousel
-      :per-page="1"
-      :navigation-enabled="true"
-      :pagination-enabled="false"
-      navigation-next-label="›"
-      navigation-prev-label="‹"
-    >
-      <Slide v-for="slide in slides" :key="slide.key" class="wrapper">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-5 content">
-              <div tabindex="-1" class="col-md-12 show-mobile">
-                <Notebook :background="slide.screen" />
-              </div>
-              <div class="text-and-button">
-                <p tabindex="0">
-                  {{ slide.content }}
-                </p>
-                <nuxt-link to="/cases" class="btn">
-                  {{ $t('cases.know') }}
-                </nuxt-link>
-              </div>
-            </div>
-            <div tabindex="-1" class="col-md-7 show-desktop">
-              <Notebook :background="slide.screen" />
-            </div>
-          </div>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h2 tabindex="0">
+            Conheça os nossos cases
+          </h2>
+          <p>Veja os nossos cases em detalhes</p>
         </div>
-      </Slide>
-    </Carousel>
+        <div class="col">
+          <img
+            src="~/assets/images/atelie-de-software-cases.jpg"
+            alt="Conheça nossos cases."
+            class="casesImage"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-import Notebook from '@/components/Notebook'
-
-export default {
-  name: 'Cases',
-  components: {
-    Notebook
-  },
-  data() {
-    return {
-      swiperOption: {
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev'
-        }
-      },
-      slides: [
-        {
-          key: 0,
-          content: this.$t('cases.educadora'),
-          screen: require('@/assets/images/educadora.png')
-        },
-        {
-          key: 1,
-          content: this.$t('cases.conlicita'),
-          screen: require('@/assets/images/conlicitacao.png')
-        },
-        {
-          key: 2,
-          content: this.$t('cases.granatum'),
-          screen: require('@/assets/images/granatum.png')
-        },
-        {
-          key: 3,
-          content: this.$t('cases.lumiar'),
-          screen: require('@/assets/images/lumiar.png')
-        },
-        {
-          key: 4,
-          content: this.$t('cases.dsop'),
-          screen: require('@/assets/images/dsop.png')
-        },
-        {
-          key: 5,
-          content: this.$t('cases.vagas'),
-          screen: require('@/assets/images/vagas.png')
-        },
-        {
-          key: 6,
-          content: this.$t('cases.synergia'),
-          screen: require('@/assets/images/synergia.png')
-        }
-      ]
-    }
-  }
-}
-</script>
 
 <style lang="scss" scoped>
 #cases {
   margin-bottom: 50px;
-}
-.row {
-  padding-left: 0;
+  h2 {
+    font-size: 60px;
+    line-height: 60px;
+    text-transform: uppercase;
+    margin-bottom: 40px;
+  }
+  .casesImage {
+    height: 100%;
+  }
 }
 p {
   margin-bottom: 50px;
@@ -136,7 +65,6 @@ p {
 .header-cases {
   @media screen and (min-width: 787px) {
     position: absolute;
-    left: 20vw;
     font-size: 25vw;
   }
   @media screen and (max-width: 786px) {
