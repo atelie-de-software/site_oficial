a3eec64376f9878aa399724beafd24be269de306
Jeff Albuquerque <jeff@MacBook-Pro-de-Jeff.local>
2020-06-08 12:43:10 -0300

    Nova section Educadorafm na tela de Cases.

--- components/Educadora.vue
+++ components/Educadora.vue
@@ -1,52 +1,32 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-6">
-        <img
-          src="~/assets/images/educadora-print.png"
-          class="show-desktop"
-          alt
-        />
-      </div>
-      <div class="col-md-6 center">
-        <img src="~/assets/images/educadora-logo.png" alt />
-        <img
-          src="~/assets/images/educadora-print.png"
-          class="show-mobile"
-          alt
-        />
-        <p>
-          {{ $t('educadora.message') }} (
-          <a href="www.educadorafm.com.br">www.educadorafm.com.br</a>)
-          {{ $t('educadora.description') }}
-        </p>
-        <p>{{ $t('educadora.descriptionII') }}</p>
+  <section class="sectionsCases">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <img
+            src="~/assets/images/educadora-logo.png"
+            alt="Educadorafm"
+            width="120"
+            class="logo"
+          />
+          <p>Estação de Rádio do grupo Bandeirantes de comunicação.</p>
+          <p>
+            Criamos o novo site com gerenciamento de conteúdo totalmente
+            dinâmico e com todos os jogos da rádio.
+          </p>
+          <p>
+            Criamos o aplicativo da rádio gamificado nas plataforma nas lojas
+            Android e IOS.
+          </p>
+        </div>
+        <div class="col-md-8">
+          <img
+            src="~/assets/images/atelie-de-software-educadorafm.png"
+            class="show-desktop"
+            alt
+          />
+        </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
-
-<style lang="scss" scoped>
-.center {
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  padding-bottom: 60px;
-}
-
-.margin-negative {
-  margin-top: -60px;
-}
-.relative {
-  position: relative;
-}
-.float-logo {
-  position: absolute;
-  bottom: 30px;
-  left: 0;
-}
-.container {
-  margin-top: 100px;
-  margin-bottom: 165px;
-}
-</style>
