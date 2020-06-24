f55bd0d70bc28018dc9ec331bf3e504619daef6a
Jeff Albuquerque <jeff@webgoal.com.br>
2020-06-24 12:46:59 -0300

    Pagina da Lumiar nos Cases.

---
+++ pages/cases/lumiar.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="detailsCase">
+    <div class="main">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-5 contentSection">
+            <img
+              src="~/assets/images/atelie-de-software-lumiar-logo.svg"
+              alt="Plataforma Lumiar"
+              class="logo-lumiar"
+            />
+            <h2>Desenvolvimento de plataforma Lumiar</h2>
+            <p>
+              Rede de Escolas Lumiar que possui uma metodologia de ensino
+              inovadora.
+            </p>
+            <p class="details">
+              <img
+                src="~/assets/images/lumiar/atelie-de-software-icon-time.svg"
+                alt="Conlicitação - Consórcio nacional de licitações do Brasil"
+              />
+              Duração: + de 2 anos
+            </p>
+            <p class="details">
+              <img
+                src="~/assets/images/lumiar/atelie-de-software-icon-team.svg"
+                alt="Conlicitação - Consórcio nacional de licitações do Brasil"
+              />
+              Equipe: 6 pessoas
+            </p>
+          </div>
+          <div class="col-md-7">
+            <img
+              src="~/assets/images/Lumiar/ateleie-de-software-telas-lumiar.png"
+              alt="Lumiar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="challenge color color-lumiar">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-5 order-first contentSection">
+            <h2>Desafios</h2>
+            <p>
+              Criar uma plataforma educacional digital que implementa a
+              Metodologia Lumiar.
+            </p>
+          </div>
+          <div class="col-md-7 order-last">
+            <img
+              src="~/assets/images/lumiar/ateleie-de-software-tela-lumiar-2.png"
+              alt="Lumiar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="solution">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 contentSection">
+            <h2>Solução</h2>
+            <p>
+              Criar ferramentas, como o mosaico digital e chamadas por vídeo,
+              para suportar o processo de ensino-aprendizagem dos estudantes das
+              escolas da rede.
+            </p>
+            <p>Para este projeto usamos:</p>
+            <p>Ruby on Rails</p>
+          </div>
+          <div class="col-md-8">
+            <img
+              src="~/assets/images/lumiar/ateleie-de-software-telas-lumiar-3.png"
+              alt="Lumiar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="screen color color-lumiar">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <img
+              src="~/assets/images/lumiar/ateleie-de-software-telas-lumiar-4.png"
+              alt="Lumiar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="result">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 contentSection">
+            <h2>Resultados</h2>
+            <p>
+              Desenvolvemos a plataforma digital da Lumiar utilizada em escolas
+              no Brasil e na Europa, que compreende todas as etapas e
+              ferramentas de ensino esperadas. Hoje um time do Ateliê trabalha
+              em constantes aprimoramentos da plataforma.
+            </p>
+          </div>
+          <div class="col-md-6 offset-md-2">
+            <img
+              src="~/assets/images/Lumiar/ateleie-de-software-telas-lumiar.png"
+              alt="Lumiar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
