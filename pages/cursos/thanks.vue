895d9ee5af73b556665c2d5ddf3afbfb1122ce1f
Jeff Albuquerque <jeff@webgoal.com.br>
2020-09-14 13:46:46 -0300

    Icones na página de obrigado.

--- pages/cursos/thanks.vue
+++ pages/cursos/thanks.vue
@@ -1,8 +1,41 @@
 <template>
   <div class="thanks">
     <div class="container text-center">
-      <h1>Obrigado!</h1>
+      <h1>Muito obrigado!</h1>
       <h3>Recebemos a sua inscrição.</h3>
+      <p>
+        Nos acompanhe nas redes sociais
+      </p>
+      <div class="row">
+        <div class="col">
+          <a href="https://www.instagram.com/atelie.software/">
+            <img
+              src="~/assets/images/icon-insta.png"
+              alt="Visite nosso Instagram"
+            />
+          </a>
+          <a href="https://www.facebook.com/atelie.software">
+            <img
+              src="~/assets/images/icon-face.png"
+              alt="Visite nosso Facebook"
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/atelie-de-software/?originalSubdomain=br"
+          >
+            <img
+              src="~/assets/images/icon-linkedin.png"
+              alt="Visite nosso Linkeding"
+            />
+          </a>
+          <a href="https://www.youtube.com/channel/UCAix6EqJM_haS2jP1jUjFiw">
+            <img
+              src="~/assets/images/icon-youtube.png"
+              alt="Visite nosso Canal no Youtube"
+            />
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
